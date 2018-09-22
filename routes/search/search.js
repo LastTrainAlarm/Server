5a6675c67ff5b786c3ef8bfc55840afc8cf3085e
jiwonsim <shark.cametotea@gmail.com>
2018-09-22 16:52:21 +0900

    search route edited

--- routes/search/search.js
+++ routes/search/search.js
@@ -3,22 +3,30 @@ var router = express.Router();
 var db = require('../../module/pool.js'); 
 var scretkey = require('../../config/secretKey.js')
 var sdata = require('../../data/seoul_subway_info.json'); 
+var querystring = require('querystring');
+
+
 
 router.get('/:keyword', async function(req, res) {
-    let keyword = req.params.keyword; 
-    if(!keyword)
-    {
-        res.status(400).send({
-            message : "Incorrect Input"
-        }); 
-    }
-    else {
-        for (let i=0; i<sdata.DATA.length; i++){
-            if(sdata.DATA[i].station_nm == keyword){
-               console.log(sdata.DATA[i]);  
-            }
-        }
-    }
+    console.log('시발');
+//    querystring.unescape(keyword);
+    // let keyword = req.params.keyword; 
+    
+
+    console.log(keyword);
+    // if(!keyword)
+    // {
+    //     res.status(400).send({
+    //         message : "Incorrect Input"
+    //     }); 
+    // }
+    // else {
+    //     for (let i=0; i<sdata.DATA.length; i++){
+    //         if(sdata.DATA[i].station_nm == keyword){
+    //            console.log(sdata.DATA[i]);  
+    //         }
+    //     }
+    // }
 });
 
 module.exports = router;
