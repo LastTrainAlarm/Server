e5bb21a8492478656ed6788c575d1051e89ea0c4
JHwaa <wjdghk1565@naver.com>
2018-09-26 20:44:26 +0900

    new get

--- routes/fav/favorite.js
+++ routes/fav/favorite.js
@@ -34,25 +34,39 @@ router.post('/', async function(req, res) {
                 });
             }
             else {
-                let checkFav = 'SELECT * FROM lasttrain.favorite' 
-                let checkFavRes = await db.queryParam_None(checkFav);
-
-                let pushFav = `
-                INSERT INTO lasttrain.favorite(fav_name, user_idx)
-                VALUES(?, ?)
-                `;
-                let pushFavResult = await db.queryParam_Arr(pushFav, [keyword, user_idx]);
-                console.log(checkFavRes);
-                if(!pushFavResult) {
-                    res.status(500).send({
-                        "message" : "Internal Server Error"
-                    });
+                let checkStn = `
+                SELECT *
+                FROM lasttrain.favorite
+                WHERE keyword = ?`;
+                let checkStnResult = await db.queryParam_Arr(checkStn, [keyword]);
+                
+                if (!checkStnResult) { // 페이보릿에 역 없으면 즐찾 추가
+                    let pushFav = `
+                    INSERT INTO lasttrain.favorite(fav_name, user_idx)
+                    VALUES(?, ?)
+                    `;
+                    let pushFavResult = await db.queryParam_Arr(pushFav, [keyword, user_idx]);
+                    if(!pushFavResult) {
+                        res.status(500).send({
+                            "message" : "Internal Server Error"
+                        });
+                    }
+                    else {
+                        console.log(pushFavResult);
+                        res.status(201).send ({
+                            "message" : "Successfully register favorite station"
+                        });
+                    }
                 }
-                else {
-                    res.status(201).send ({
-                        "message" : "Successfully register favorite station"
-                    });
+                else { // 역 있으면 즐찾 해제
+                    let delFav = `
+                    DELETE FROM lasttrain.favorite
+                    WHERE keyword = ?
+                    `;
+                    let delFavResult = await db.queryParam_Arr(delFav, [keyword]);
+                    console.log(delFavResult);
                 }
+                
             }
         }
         else { // 등록된 역 이름이 아닐 경우
@@ -61,6 +75,9 @@ router.post('/', async function(req, res) {
             });
         }
     }
+    let checkFav = 'SELECT * FROM lasttrain.favorite' 
+    let checkFavRes = await db.queryParam_None(checkFav);
+    console.log(checkFavRes);
  });
 
  router.get('/', async function(req, res) {
@@ -86,7 +103,6 @@ router.post('/', async function(req, res) {
             });
         }
         else {
-            console.log(getFavResult);
             res.status(201).send ({
                 "message" : "Successfully register favorite station",
                 "data" : getFavResult.fav_name // 이것이 아닌가봉가!!!
